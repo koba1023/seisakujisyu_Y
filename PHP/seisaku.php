27bbda87c24405491a675aa6065e502f0464d7ad
MATUMOTO Mitou <kd1336124@st.kobedenshi.ac.jp>
2023-12-04 11:05:18 +0900

    変更

---
+++ PHP/seisaku.php
@@ -0,0 +1,29 @@
+<?php 
+require_once __DIR__. '/_common/css/seisaku.css';
+
+if(isset($_SESSION['login_error'])){
+    echo '<p class="error_class">'. $_SESSION['login_error']. '</p>';
+    unset($_SESSION['login_error']);
+}else{
+    echo '<p>利用するにあたってはログインしてください。</p>';
+}
+?>
+<form method="POST" action="./seisaku_db.php">
+    <table>
+        <tr>
+            <td>Eメール</td>
+            <td><input type="text" name="userId" required></td>
+        </tr>
+        <tr>
+            <td>パスワード</td>
+            <td><input type="password" name="password" required></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="ログイン"></td>
+        </tr>
+    </table>
+</form>
+<a href="./signup.php"><span class="button_image">新規登録はこちらから</span></a>
+<?php
+require_once __DIR__.'/../footer.php';
+?>
