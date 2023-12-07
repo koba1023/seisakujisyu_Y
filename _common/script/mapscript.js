50a2077d61a86eaa37dc5abd4ecfe3f3f7e3e536
MATUMOTO Mitou <kd1336124@st.kobedenshi.ac.jp>
2023-12-07 12:08:17 +0900

    地図のみ追加

--- _common/script/mapscript.js
+++ _common/script/mapscript.js
@@ -1,57 +1,76 @@
 'use strict';
 
-//位置データ取得
 let map, infoWindow;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 6,
+        zoom: 12,
     });
     infoWindow = new google.maps.InfoWindow();
 
-    const locationButton = document.createElement("button");
+    // 現在位置の取得
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
 
-    locationButton.textContent = "Pan to Current Location";
-    locationButton.classList.add("custom-map-control-button");
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(
-        locationButton
-    );
-    locationButton.addEventListener("click", () => {
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
-                    };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("現在位置");
+                infoWindow.open(map);
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 
-                    infoWindow.setPosition(pos);
-                    infoWindow.setContent("Location found.");
-                    infoWindow.open(map);
-                    map.setCenter(pos);
-                },
-                () => {
-                    handleLocationError(true, infoWindow, map.getCenter());
+    // 避難場所の検索
+    const service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(
+        {
+            location: map.getCenter(),
+            radius: 5000, // 検索半径（メートル）
+            keyword: "避難場所", // 検索キーワード
+        },
+        (results, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (let i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
                 }
-            );
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
+            }
         }
-    });
+    );
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(
         browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
+            ? "エラー: 位置情報の取得に失敗しました。"
+            : "エラー: ブラウザが位置情報取得をサポートしていません。"
     );
     infoWindow.open(map);
 }
 
-window.initMap = initMap;
-//データ取得
+function createMarker(place) {
+    const marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location,
+        title: place.name,
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+        content: place.name,
+    });
+
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
+}
