323ed0016350b3ea92655485eb89d0e80f86df3d
MATUMOTO Mitou <kd1336124@st.kobedenshi.ac.jp>
2023-12-15 13:38:36 +0900

    ちょっと訂正

--- _common/script/mapscript.js
+++ _common/script/mapscript.js
@@ -4,25 +4,63 @@ let map, geocoder;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
+        center: { lat: 35.6804, lng: 139.7690 },
         zoom: 12,
     });
-    geocoder = new google.maps.Geocoder();
+    infoWindow = new google.maps.InfoWindow();
+
+    // 現在位置の取得
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("現在位置");
+                infoWindow.open(map);
+                map.setCenter(pos);
+
+                // 避難場所の検索
+                const service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(
+                    {
+                        location: pos,
+                        radius: 5000, // 検索半径（メートル）
+                        keyword: "避難所", // 検索キーワード
+                    },
+                    (results, status) => {
+                        if (status === google.maps.places.PlacesServiceStatus.OK) {
+                            for (let i = 0; i < results.length; i++) {
+                                createMarker(results[i]);
+                            }
+                        }
+                    }
+                );
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
-function searchLocation() {
-    const address = document.getElementById('searchInput').value;
+function createMarker(place) {
+    const marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        title: place.name,
+    });
 
-    geocoder.geocode({ 'address': address }, (results, status) => {
-        if (status === 'OK') {
-            map.setCenter(results[0].geometry.location);
-            const marker = new google.maps.Marker({
-                map,
-                position: results[0].geometry.location,
-                title: address,
-            });
-        } else {
-            alert('位置情報が見つかりませんでした: ' + status);
-        }
+    const infowindow = new google.maps.InfoWindow({
+        content: place.name,
+    });
+
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
     });
 }
